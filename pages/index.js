7399775e66893db8805d003f01a496c4eb78b0c9
CelsonF <celson338540@gmail.com>
2021-03-10 00:14:15 -0300

    Updates Login and create structure page Register

--- pages/index.js
+++ pages/index.js
@@ -2,34 +2,46 @@ import Link from "next/link";
 import Image from 'next/image'
 import styles from "../styles/Home.module.css";
 
+
 export default function Home() {
   return (
-      <div className={styles.container}>
-     
-       <Image 
-        src="/landing.svg"
+    <div className={styles.container}>
+
+      <Image
+        className={styles.imgLogin}
+        src="/cadastro.svg"
         alt="Landing page image"
-        height="500"
-        width="500"/>
+        width="730"
+        height="530"
+        draggable="false"
+      />
 
-        <div className={styles.login}>
-          <h1>Bem-vindo!</h1>
+      <div className={styles.login}>
+        <h1 className={styles.title} >Bem-vindo!</h1>
 
-            <input type="text" className={styles.cpf} name="cpf" id="cpf" placeholder="CPF"/>
-            <input type="password" className={styles.password} name="password" id="password" placeholder="Senha"/>
-          
-          <div className={styles.footerLogin}>
-
-            <h2>Não possui uma conta? <br/>
-              <Link href="/cadastro">
-                <a>Crie uma!</a>
-              </Link>
-            </h2>
-
-            <button className={styles.btnPrimary}>Acessar</button>
-
+        <div className={styles.inputText}>
+          <div className={styles.fields}>
+            <input type="text" className={styles.cpf} name="cpf" id="cpf" placeholder="XXX.XXX.XXX-XX" />
+            <label htmlFor="cpf"> cpf </label>
+          </div>
+          <div className={styles.fields}>
+            <input type="password" className={styles.password} name="password" id="password" placeholder="Senha" />
+            <label htmlFor="password"> senha </label>
           </div>
         </div>
+       
+        <div className={styles.footerLogin}>
+
+          <h2>Não possui uma conta? <br />
+            <Link href="/register">
+              <a>Crie uma!</a>
+            </Link>
+          </h2>
+
+          <button className={styles.btnPrimary}>Acessar</button>
+
+        </div>
       </div>
+    </div>
   )
 }
---
+++ pages/register.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import Image from 'next/image';
+import styles from "../styles/Register.module.css";
+
+export default function Register() {
+    return (
+       <div className={styles.container}>
+           <h1>Hello</h1>
+
+       </div>
+    )
+}
