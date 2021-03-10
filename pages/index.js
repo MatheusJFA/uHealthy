0bca382f24ab01d680a14ba7260d988903080699
Matheus José Ferreira de Aguiar <MatheusJFA@outlook.com>
2021-03-09 21:07:25 -0300

    Iniciação da primeira tela

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,35 @@
-import styles from '../styles/Home.module.css'
+import Link from "next/link";
+import Image from 'next/image'
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-       <h1>Olá</h1>
-    </div>
+      <div className={styles.container}>
+     
+       <Image 
+        src="/landing.svg"
+        alt="Landing page image"
+        height="500"
+        width="500"/>
+
+        <div className={styles.login}>
+          <h1>Bem-vindo!</h1>
+
+            <input type="text" className={styles.cpf} name="cpf" id="cpf" placeholder="CPF"/>
+            <input type="password" className={styles.password} name="password" id="password" placeholder="Senha"/>
+          
+          <div className={styles.footerLogin}>
+
+            <h2>Não possui uma conta? <br/>
+              <Link href="/cadastro">
+                <a>Crie uma!</a>
+              </Link>
+            </h2>
+
+            <button className={styles.btnPrimary}>Acessar</button>
+
+          </div>
+        </div>
+      </div>
   )
 }
