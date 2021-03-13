8553cad91723c0cbd0319fa8af213cf3ee2160b9
Matheus José Ferreira de Aguiar <MatheusJFA@outlook.com>
2021-03-13 10:39:05 -0300

    adição do useState

--- pages/index.js
+++ pages/index.js
@@ -25,11 +25,11 @@ export default function Home() {
 
         <div className={styles.inputText}>
           <div className={styles.fields}>
-            <input type="text"  name="cpf" id="cpf" placeholder="XXX.XXX.XXX-XX" />
+            <input type="text"  name="cpf" id="cpf" value={cpf} OnChangeText={setCPF} placeholder="XXX.XXX.XXX-XX" />
             <label htmlFor="cpf"> cpf </label>
           </div>
           <div className={styles.fields}>
-            <input type="password"  name="password" id="password" placeholder="Senha" />
+            <input type="password"  name="password" id="password" value={password} OnChangeText={setPassword} placeholder="Senha" />
             <label htmlFor="password"> senha </label>
           </div>
         </div>
