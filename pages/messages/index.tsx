b1051f24d584b026071361dc385114ef24e5afaa
Matheus José Ferreira de Aguiar <MatheusJFA@outlook.com>
2021-03-20 10:48:14 -0300

    Correção

--- pages/messages/index.tsx
+++ pages/messages/index.tsx
@@ -1,9 +1,9 @@
 export default class Messages {
-  public static readonly MSG_E000 = (campo: string) => `${campo} já cadastrado`;
-  public static readonly MSG_E001 = (campo: string) => `${campo} inexistente`;
-  public static readonly MSG_E002 = (campo: string) => `O campo ${campo} é inválido!`;
-  public static readonly MSG_E003 = (campo: string) => `O campo ${campo} é necessário!`;
-  public static readonly MSG_E004 = (campo: string, minimo: number, maximo: number) => `O campo ${campo} deve ter no mínimo ${minimo} e no máximo ${maximo} caracteres!`;
+  public static readonly MSG_E000 = (field: string) => `${field} já cadastrado`;
+  public static readonly MSG_E001 = (field: string) => `${field} inexistente`;
+  public static readonly MSG_E002 = (field: string) => `O campo ${field} é inválido!`;
+  public static readonly MSG_E003 = (field: string) => `O campo ${field} é necessário!`;
+  public static readonly MSG_E004 = (field: string, min: number, max: number) => `O campo ${field} deve ter no mínimo ${min} e no máximo ${max} caracteres!`;
   public static readonly MSG_E005 = "Usuário ou senha inválidos";
   public static readonly MSG_E006 = "Usuário não autorizado";
 
