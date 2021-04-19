750436b98fe9a93830f03a41d642278cd1487c86
Matheus José Ferreira de Aguiar <MatheusJFA@outlook.com>
2021-04-19 19:18:46 -0300

    Correção

--- components/Modal/Vaccine/index.tsx
+++ components/Modal/Vaccine/index.tsx
@@ -55,62 +55,88 @@ export default function Modal(property: IModal) {
       {property.showModal &&
         <div className="background">
           <div className="conteudoModal">
+            <Input
+              label="Nome da Vacina"
+              id="vaccineName"
+              value={vaccineName}
+              onChange={e => setVaccineName(e.target.value)}
+              placeholder="Digite o nome da vacina"
+              type="text" />
+
+            <Input
+              label="Tipo da Vacina"
+              id="vaccineType"
+              value={vaccineType}
+              onChange={e => setVaccineType(e.target.value)}
+              placeholder="Digite o tipo da vacina"
+              type="text" />
+
+            <Input
+              label="Fabricante da Vacina"
+              id="vaccineManufacturer"
+              value={vaccineManufacturer}
+              onChange={e => setVaccineManufacturer(e.target.value)}
+              placeholder="Digite o fabricante da vacina"
+              type="text" />
+
+            <div className="flex flex-row items-center justify-items-center">
               <Input
-                label="Nome da Vacina"
-                id="vaccineName"
-                value={vaccineName}
-                onChange={e => setVaccineName(e.target.value)}
-                placeholder="Digite o nome da vacina"
-                type="text" />
-
-              <Input
-                label="Tipo da Vacina"
-                id="vaccineType"
-                value={vaccineType}
-                onChange={e => setVaccineType(e.target.value)}
-                placeholder="Digite o tipo da vacina"
-                type="text" />
-
-              <Input
-                label="Fabricante da Vacina"
-                id="vaccineManufacturer"
-                value={vaccineManufacturer}
-                onChange={e => setVaccineManufacturer(e.target.value)}
-                placeholder="Digite o fabricante da vacina"
-                type="text" />
-
-              <div className="inline-flex flex-w flex-row items-center justify-items-center">
-                <select>
-                  <option value="1">1ª Dose</option>
-                  <option value="2">2ª Dose</option>
-                  <option value="3">3ª Dose</option>
-                  <option value="R1">1ª reforço</option>
-                  <option value="R2">2ª reforço</option>
-                </select>
-
-                <Input
-                  label="Dose da vacina"
-                  id="vaccineDoses"
-                  value={vaccineDoses}
-                  onChange={e => setVaccineDoses(e.target.value)}
-                  placeholder=""
-                  type="date" />
-              </div>
-
-              <Input
-                label="Local da vacinação"
-                id="vaccinationLocal"
+                label="1ª Dose"
+                id="vaccineDoses"
                 value={vaccineDoses}
-                onChange={e => setVaccinationLocal(e.target.value)}
-                placeholder="Digite o posto/hospítal no qual você tomou a vacina"
-                type="text" />
+                onChange={e => setVaccineDoses(e.target.value)}
+                placeholder=""
+                type="date" />
 
-              <div className="flex flex-row items-center justify-between p-5 bg-white border-t border-gray-200 rounded-bl-lg rounded-br-lg">
-                <button className="font-semibold text-gray-600" onClick={() => close()}>Cancel</button>
-                <button className="px-4 py-2 text-white font-semibold bg-red-500 rounded" onClick={() => Save()}>Salvar</button>
-              </div>
+              <Input
+                label="2ª Dose"
+                id="vaccineDoses"
+                value={vaccineDoses}
+                onChange={e => setVaccineDoses(e.target.value)}
+                placeholder=""
+                type="date" />
+
+              <Input
+                label="3ª Dose"
+                id="vaccineDoses"
+                value={vaccineDoses}
+                onChange={e => setVaccineDoses(e.target.value)}
+                placeholder=""
+                type="date" />
+            </div>
+
+            <div className="flex flex-row items-center justify-items-center">
+              <Input
+                label="1ª Dose de Reforço"
+                id="vaccineDoses"
+                value={vaccineDoses}
+                onChange={e => setVaccineDoses(e.target.value)}
+                placeholder=""
+                type="date" />
+
+              <Input
+                label="2ª Dose de Reforço"
+                id="vaccineDoses"
+                value={vaccineDoses}
+                onChange={e => setVaccineDoses(e.target.value)}
+                placeholder=""
+                type="date" />
+            </div>
+
+            <Input
+              label="Local da vacinação"
+              id="vaccinationLocal"
+              value={vaccineDoses}
+              onChange={e => setVaccinationLocal(e.target.value)}
+              placeholder="Digite o posto/hospítal no qual você tomou a vacina"
+              type="text" />
+
+            <div className="flex flex-row items-center justify-between p-5 bg-white border-t border-gray-200 rounded-bl-lg rounded-br-lg">
+              <button className="font-semibold text-gray-600" onClick={() => close()}>Cancel</button>
+              <button className="px-4 py-2 text-white font-semibold bg-red-500 rounded" onClick={() => Save()}>Salvar</button>
             </div>
           </div>
+        </div>
       }
     </>
   );
