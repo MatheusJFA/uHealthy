383ba4aa786d7a3de28a5a5b9ec2e9e27c76e53b
Vinicius Marinho <48867230+ViniciusAlexsander@users.noreply.github.com>
2021-05-08 10:08:21 -0300

    cadastro de dependentes

---
+++ pages/api/dependent/index.tsx
@@ -0,0 +1,66 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import * as Yup from 'yup';
+
+import Messages from '../../../utils/messages';
+
+import { PrismaClient, Prisma } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default async (request: NextApiRequest, response: NextApiResponse) => {
+
+  try {
+    var today = new Date();
+
+    if (request.method === "POST") {
+      const schema = Yup.object().shape({
+        userId: Yup.number().required(),
+        name: Yup.string().required(),
+        birthDate: Yup.date().required().max(today),
+      });
+
+      if (!(await schema.isValid(request.body))) return response.status(400).json({ error: Messages.MSG_A002 });
+
+      let { userId, name, birthDate } = request.body;
+
+      const userExists = await prisma.user.findFirst(
+        {
+          where: {
+            id: userId
+          },
+        });
+
+      if (!userExists) return response.status(400).json({ error: Messages.MSG_E001("Usuario") });
+
+      birthDate = new Date(birthDate).toISOString() as any;
+
+      const dependentExists = await prisma.dependent.findFirst(
+        {
+          where: {
+            AND: [
+              { userId },
+              { name },
+            ],
+          }
+        }
+      );
+
+      if (dependentExists) return response.status(400).json({ error: Messages.MSG_E000("Dependente") });
+
+      const dependent = await prisma.dependent.create({
+        data: {
+          userId,
+          name,
+          birthDate
+        },
+      });
+
+      await prisma.$disconnect()
+
+      return response.status(200).send({ dependente: dependent });
+    }
+  } catch (error) {
+    response.status(500).json({ error: error });
+  }
+}
